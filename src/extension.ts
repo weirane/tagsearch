47d8eac152aca94cd1967f1708711ed216a53eab
Wang Ruochen <wrc@ruo-chen.wang>
2023-10-30 01:06:03 -0400

    treeview content

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
-import { abort } from "process";
 import * as vscode from "vscode";
+import { TreeItem, TreeItemCollapsibleState } from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -19,27 +19,59 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.window.registerTreeDataProvider("tagsearch-results", dataProvider);
 }
 
-class TagSearchView {
-  constructor() {
-    // this.treeView = vscode.window.createTreeView("yourViewId", {
-    //   treeDataProvider: new TagSearchViewData(),
-    // });
-  }
+interface Function {
+  kind: "func";
+  signature: string;
+  tags: string[];
+  // need location info (for click to jump)
 }
-
-class Result implements vscode.TreeItem {}
+interface File {
+  kind: "file";
+  path: string;
+  funcs: Function[];
+}
+type Result = Function | File;
 
 class TagSearchViewData implements vscode.TreeDataProvider<Result> {
   constructor(rootPath?: string) {}
 
-  getTreeItem(element: Result): Result {
-    abort();
-    // Return a tree item representing the element
+  getTreeItem(element: Result): TreeItem {
+    if (element.kind === "file") {
+      const ti = new TreeItem("file " + element.path, TreeItemCollapsibleState.Expanded);
+      // TODO: prettier output. icons?
+      ti.contextValue = "file";
+      return ti;
+    } else {
+      const ti = new TreeItem("function " + element.signature);
+      // TODO: prettier output. display the matched tag
+      return ti;
+    }
   }
 
-  getChildren(element: Result): Result[] {
-    // Return an array of child elements
-    return [];
+  getChildren(element?: Result): Thenable<Result[]> {
+    if (element === undefined) {
+      // entered the extension for the first time.
+      // perform search on all files and return a list of files with functions
+      // can search in promise to be async. need to save results?
+      const sample: File = {
+        kind: "file",
+        path: "path/to/file.py",
+        funcs: [
+          {
+            kind: "func",
+            signature: "def asmatrix()",
+            tags: ["conversion", "matrix"],
+          },
+        ],
+      };
+      return Promise.resolve([sample]);
+    } else if (element.kind === "file") {
+      // search within file (again)?
+      return Promise.resolve(element.funcs);
+    } else {
+      // got to functions. should be unreachable
+      return Promise.resolve([]);
+    }
   }
 }
 
