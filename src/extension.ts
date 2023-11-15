4c7bda431933ab432053888c629652579d50c353
bob <nlanuzo@masonlive.gmu.edu>
2023-11-15 14:44:07 -0500

    Added persistent data access

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+// import { File } from "buffer";
 import * as vscode from "vscode";
 import { TreeItem, TreeItemCollapsibleState } from "vscode";
 
@@ -17,12 +18,202 @@ export function activate(context: vscode.ExtensionContext) {
 
   const dataProvider = new TagSearchViewData(rootPath);
   vscode.window.registerTreeDataProvider("tagsearch-results", dataProvider);
+
+  function getData() {
+    let json_data = context.globalState.get("json_data", "{'list_of_files': []}");
+    return json_data
+  }
+
+  // Block shows get data, set data, and deleting data
+
+  // console.log(getData())
+  // updateData("test")
+  // console.log(getData())
+  // deleteData()
+  // console.log(getData())
+  
+  function updateData(new_data: string) {
+    context.globalState.update("json_data", new_data)
+  }
+
+  function deleteData() {
+    context.globalState.update("json_data", "{'list_of_files': []}");
+  }
+
+  function doesFileExist(json: any, file_name: string): boolean {
+    return json.list_of_files.some((item: any) => item.path === file_name);
+}
+
+// This blcok shows checking if a file exists
+
+// console.log("doesfileexist")
+// console.log(doesFileExist({"list_of_files": [{'file_path': "test.py"}]}, "test.py"))
+// console.log("doesfileexist")
+
+// Function to add the name if it doesn't exist
+function addFileIfNotExists(main_json: any, file: File): void {
+    if (!doesFileExist(main_json, file.path)) {
+      main_json.list_of_files.push(file);
+        console.log(`Added "${file.path}" to the list.`);
+    } else {
+        console.log(`"${file.path}" already exists in the list.`);
+    }
+}
+
+const sample: File = {
+  kind: "file",
+  path: "path/to/file.py",
+  funcs: [
+    {
+      kind: "func",
+      name: "name",
+      signature: "def asmatrix()",
+      tags: ["conversion", "matrix"],
+      returns: ["return_var"],
+      description: "fake description",
+      file_path: "path/to/file.py:L12-24"
+    },
+  ],
+};
+
+const sample2: File = {
+  kind: "file",
+  path: "path/to/file2.py",
+  funcs: [
+    {
+      kind: "func",
+      name: "name",
+      signature: "def asmatrix()",
+      tags: ["conversion", "matrix"],
+      returns: ["return_var"],
+      description: "fake description",
+      file_path: "path/to/file.py:L12-24"
+    },
+  ],
+};
+// const example_json = {"list_of_files": [sample]}
+// addFileIfNotExists(example_json, sample2)
+// console.log(JSON.stringify(example_json))
+
+// console.log(" ")
+
+// function combineFileFuncs(data_json: any, newFile: File): any {
+//   if (!data_json.list_of_files || !Array.isArray(data_json.list_of_files)) {
+//       console.error("Invalid data_json format. It should have a list_of_files property.");
+//       return data_json;
+//   }
+
+//   const existingFileIndex = data_json.list_of_files.findIndex((file: File) => file.path === newFile.path);
+
+//   if (existingFileIndex !== -1) {
+//       const existingFile = data_json.list_of_files[existingFileIndex];
+//       const combinedFuncs = [...existingFile.funcs];
+
+//       newFile.funcs.forEach((newFunc: any) => {
+//           if (!combinedFuncs.some((existingFunc: { name: string }) => existingFunc.name === newFunc.name)) {
+//               console.log(newFunc.name)
+//               combinedFuncs.push({ name: newFunc.name });
+//           }
+//       });
+
+//       data_json.list_of_files[existingFileIndex] = {
+//           path: existingFile.path,
+//           funcs: combinedFuncs,
+//       };
+//   } else {
+//       data_json.list_of_files.push(newFile);
+//   }
+//   return data_json;
+// }
+
+function combineFileFuncs(data_json: any, newFile: File): any {
+  if (!data_json.list_of_files || !Array.isArray(data_json.list_of_files)) {
+      console.error("Invalid data_json format. It should have a list_of_files property.");
+      return data_json;
+  }
+
+  const existingFileIndex = data_json.list_of_files.findIndex((file: File) => file.path === newFile.path);
+
+  if (existingFileIndex !== -1) {
+      const existingFile = data_json.list_of_files[existingFileIndex];
+
+      newFile.funcs.forEach((newFunc: Function) => {
+          const existingFuncIndex = existingFile.funcs.findIndex((existingFunc: Function) => existingFunc.name === newFunc.name);
+
+          if (existingFuncIndex === -1) {
+              existingFile.funcs.push(newFunc);
+          } else {
+              // Merge additional properties of 'func' if needed
+              // In this example, assuming 'func' objects have no additional properties
+              console.log(`Func with name '${newFunc.name}' already exists in the file.`);
+          }
+      });
+
+      data_json.list_of_files[existingFileIndex] = {
+          path: existingFile.path,
+          funcs: existingFile.funcs,
+      };
+  } else {
+      data_json.list_of_files.push(newFile);
+  }
+
+  return data_json;
+}
+
+const example_json = {"list_of_files": [sample]}
+const new_json = combineFileFuncs(example_json, sample2)
+console.log(JSON.stringify(new_json))
+console.log("")
+
+  function appendNewFunction(func_name: string, func_tags: string[], func_sig: string, 
+                             func_returns: string[], func_description: string, func_file_path:string ) {
+
+    let json_string_data = context.globalState.get("json_string_data", "{'list_of_files': []}");
+    const function_to_append: Function = {
+      kind: "func",
+      name: func_name,
+      tags: func_tags,
+      signature: func_sig,
+      returns: func_returns,
+      description: func_description,
+      file_path: func_file_path
+    };
+
+
+
+
+
+  }
+
+
+
+// let json_data = JSON.parse(json_string_data)
+
+//     const function_to_append: Function = {
+//       kind: "func",
+//       name: func_name,
+//       tags: ["TAG1", "TAG2", "TAG3"],
+//       signature: "",
+//       returns: ["return1", "return2"],
+//       description: "DESCRIPTION",
+//       file_path: "FILE_PATH"
+//     };
+
+//     json_data.list_of_saved_functions.push(function_to_append);
+
+//     context.globalState.update("json_data", json_data);
+  
+  return context
 }
 
 interface Function {
   kind: "func";
+  name: string
   signature: string;
   tags: string[];
+  returns: string[];
+  description: string;
+  file_path: string;
   // need location info (for click to jump)
 }
 interface File {
@@ -42,8 +233,9 @@ class TagSearchViewData implements vscode.TreeDataProvider<Result> {
       ti.contextValue = "file";
       return ti;
     } else {
-      const ti = new TreeItem("function " + element.signature);
+      const ti = new TreeItem("function " + element.signature, TreeItemCollapsibleState.Expanded);
       // TODO: prettier output. display the matched tag
+      ti.contextValue = 'function';
       return ti;
     }
   }
@@ -53,18 +245,47 @@ class TagSearchViewData implements vscode.TreeDataProvider<Result> {
       // entered the extension for the first time.
       // perform search on all files and return a list of files with functions
       // can search in promise to be async. need to save results?
+
+      // getData
+
+      // var parsedObject = JSON.parse(jsonString);
+      // console.log("Parsed Object:", parsedObject);
+
       const sample: File = {
         kind: "file",
         path: "path/to/file.py",
         funcs: [
           {
             kind: "func",
+            name: "name",
             signature: "def asmatrix()",
             tags: ["conversion", "matrix"],
+            returns: ["return_var"],
+            description: "fake description",
+            file_path: "path/to/file.py:L12-24"
           },
         ],
       };
-      return Promise.resolve([sample]);
+
+      var jsonString = JSON.stringify(sample);
+      console.log("JSON String:", jsonString);
+
+      const sample2: File = {
+        kind: "file",
+        path: "path/to/file2.py",
+        funcs: [
+          {
+            kind: "func",
+            name: "name2",
+            signature: "def asmatrix()",
+            tags: ["conversion", "matrix"],
+            returns: ["return_var"],
+            description: "fake description",
+            file_path: "path/to/file.py:L12-24"
+          },
+        ],
+      };
+      return Promise.resolve([sample, sample2]);
     } else if (element.kind === "file") {
       // search within file (again)?
       return Promise.resolve(element.funcs);
